46398cbabfd33ecdc837cb5186af02c616cab32e
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-09-01 21:17:54 +0530

    added performance tests for vec, numpy vec in local machine and google colab

--- tests/performance/numpy_performance.py
+++ tests/performance/numpy_performance.py
@@ -2,6 +2,8 @@ import numpy as np
 import time
 import operator
 
+sizes = [2**i for i in range(11, 17)]
+
 def vec_vec_performance(size: int, func):
     start_time = time.time()
 
@@ -9,23 +11,22 @@ def vec_vec_performance(size: int, func):
     v2 = np.random.uniform(0, 1, size)
 
 
-    v3 = func(v1, v2)
+    func(v1, v2)
 
     end_time = time.time()
 
     duration = end_time - start_time
 
-    print(f"Total time for {size}k {func.__name__} is {duration * 1000:.2f} ms")
-
-sizes = [2**i for i in range(11, 17)] 
+    print(f"size= {size:>6}k | operation= {func.__name__:<5}| time= {duration * 1000:5.2f} ms")
 
 v_v_operators = [
     operator.add,
     operator.sub,
+    operator.iadd
 ]
 
 for op in v_v_operators:
-    print(f"\n----- {op.__name__.upper()} -----")
+    print(f"\n--------------------- {op.__name__.upper():<4} ---------------------")
     for size in sizes:
         vec_vec_performance(size, op)
 
@@ -34,18 +35,18 @@ def vec_scalar_performance(size: int, func):
     start_time = time.time()
     v1 = np.random.uniform(0, 1, size)
 
-    v2 = func(67, v1)
+    func(67, v1)
     end_time = time.time()
-    
+
     duration = end_time - start_time
-    print(f"Total time for {size}k {func.__name__} is {duration * 1000:.2f} ms")
+    print(f"size= {size:>6}k | operation= {func.__name__:<5}| time= {duration * 1000:5.2f} ms")
 
 v_s_operators = [
+    operator.mul,
     operator.imul,
-    operator.iadd,
 ]
 
 for op in v_s_operators:
-    print(f"\n----- {op.__name__.upper()} -----")
+    print(f"\n--------------------- {op.__name__.upper():<4} ---------------------")
     for size in sizes:
         vec_scalar_performance(size, op)
--- tests/performance/vec_performance.py
+++ tests/performance/vec_performance.py
@@ -1,51 +1,51 @@
-from vec import Vec
+from src.vec.vec import Vec
 import time
 import operator
 
+sizes = [2**i for i in range(11, 17)]
+
 def vec_vec_performance(size: int, func):
-    start_time = time.time()
 
     v1 = Vec.uniform(size)
     v2 = Vec.uniform(size)
 
+    start_time = time.time()
 
-    v3 = func(v1, v2)
+    func(v1, v2)
 
     end_time = time.time()
 
     duration = end_time - start_time
 
-    print(f"Total time for {size}k {func.__name__} is {duration * 1000:.2f} ms")
-
-sizes = [2**i for i in range(11, 17)] 
+    print(f"size= {size:>6}k | operation= {func.__name__:<5}| time= {duration * 1000:5.2f} ms")
 
 v_v_operators = [
     operator.add,
     operator.sub,
-]
-
-for op in v_v_operators:
-    print(f"\n----- {op.__name__.upper()} -----")
-    for size in sizes:
-        vec_vec_performance(size, op)
-
-
-def vec_scalar_performance(size: int, func):
-    start_time = time.time()
-    v1 = Vec.uniform(size)
-
-    v2 = func(67, v1)
-    end_time = time.time()
-    
-    duration = end_time - start_time
-    print(f"Total time for {size}k {func.__name__} is {duration * 1000:.2f} ms")
-
-v_s_operators = [
-    operator.imul,
     operator.iadd,
 ]
 
+for op in v_v_operators:
+    print(f"\n--------------------- {op.__name__.upper():<4} ---------------------")
+    for size in sizes:
+        vec_vec_performance(size, op)
+
+def vec_scalar_performance(size: int, func):
+    v1 = Vec.uniform(size)
+
+    start_time = time.time()
+    func(67, v1)
+    end_time = time.time()
+
+    duration = end_time - start_time
+    print(f"size= {size:>6}k | operation= {func.__name__:<5}| time= {duration * 1000:5.2f} ms")
+
+v_s_operators = [
+    operator.mul,
+    operator.imul,
+]
+
 for op in v_s_operators:
-    print(f"\n----- {op.__name__.upper()} -----")
+    print(f"\n--------------------- {op.__name__.upper():<4} ---------------------")
     for size in sizes:
         vec_scalar_performance(size, op)
