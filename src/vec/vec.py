554677263ecc87720cf64cc407afde098cc03bd8
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-09-14 22:43:08 +0530

    added method to find the standard deviation of the vector

--- src/vec/vec.py
+++ src/vec/vec.py
@@ -227,6 +227,9 @@ class Vec:
         sum_square = sum(x ** 2 for x in self.elements)
         return round(math.sqrt(sum_square), 5)
 
+    def inner_product(self: Self, other: Self) -> float:
+        return sum(x * i for x, i in zip(self.elements, other.elements))
+
     def mean(self: Self) -> float:
         """
         Method to calculate the mean/average of the vector.
@@ -262,7 +265,24 @@ class Vec:
         # return Vec(de_mean)
 
         return self - self.mean() * self.ones(len(self.elements))
-        
+
+    def std(self: Self) -> float:
+        """
+        Method to calculate the standard deviation.
+        For a vector 'x' with 'n' elements x1, x2, ... xn.
+
+        std(x) = √(((x1 - mean(x))^2 + (x2 - mean(x))^2 + ... + (xn - mean(x))^2) / n)
+
+        std(x) = ||x̄|| / √n
+        where ||x̄|| is the norm of the De-mean of vector 'x'.
+
+        Args:
+            Vec: The vector to calculate the standard deviation.
+
+        Returns:
+            Standard deviation (float): ||x̄|| / √n
+        """
+        return round(self.demean().norm() / math.sqrt(len(self.elements)), 5)
 
 if __name__ == "__main__":
     v1 = Vec([1, 2, 3])
@@ -307,8 +327,11 @@ if __name__ == "__main__":
     v8 = Vec([-3, 2, -1, 1, -1])
     print(v8.norm())
 
-    mean = v1.mean()
-    print(mean)
+    v1.inner_product(Vec.ones(len(v1)))
 
     v9 = Vec([10, -2, 8, 23, 6, 75])
+    print(v9.mean())
+
     print(v9.demean())
+
+    print(v9.std())
